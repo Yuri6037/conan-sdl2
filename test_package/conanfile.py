e471a1d579d4424e0ebf2cfcda3093456c226461
Yuri Edward <yuri6037@outlook.com>
2020-08-07 10:50:26 +0200

    Fixed test

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -15,8 +15,6 @@ class TestPackageConan(ConanFile):
             cmake.definitions["WITH_X11"] = self.options["sdl2"].x11
             cmake.definitions["WITH_ALSA"] = self.options["sdl2"].alsa
             cmake.definitions["WITH_PULSE"] = self.options["sdl2"].pulse
-            cmake.definitions["WITH_ESD"] = self.options["sdl2"].esd
-            cmake.definitions["WITH_ARTS"] = self.options["sdl2"].arts
             cmake.definitions["WITH_DIRECTFB"] = self.options["sdl2"].directfb
         if self.settings.os == "Windows":
             cmake.definitions["WITH_DIRECTX"] = self.options["sdl2"].directx
